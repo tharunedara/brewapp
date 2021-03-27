e7d0f11589089f6a75855943f7a87e423685e9cd
Tharun <tharunedara@gmail.com>
2021-03-27 15:40:58 +0530

    Completed

--- lib/screens/settings_form.dart
+++ lib/screens/settings_form.dart
@@ -1,6 +1,11 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/shared/constants.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/services/database.dart';
 
 class SettingsForm extends StatefulWidget {
   @override
@@ -8,86 +13,107 @@ class SettingsForm extends StatefulWidget {
 }
 
 class _SettingsFormState extends State<SettingsForm> {
-
-   final _formKey = GlobalKey<FormState>();
-   final List<String> sugars = ['0','1','2','3','4'];
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
 
   String _currentName;
   String _currentSugars;
   int _currentStrength;
 
-
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key:_formKey,
-      child: Column(
-        children: [
-          Text(
-            "Update your brew settings",
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(height: 20,),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val){
-              return val.isEmpty ? 'Please enter a name' : null;
-            },
-            onChanged: (val){
-              setState(() {
-                _currentName = val;
-              });
-            },
-          ),
-          SizedBox(height: 20,),
-          //dropdowm
-          DropdownButtonFormField(
-            decoration: textInputDecoration,
-            value: _currentSugars ?? '0',
-            onChanged: (val){
-              setState(() {
-                _currentSugars= val;
-              });
-            },
-            items: sugars.map((sugar){
-              return DropdownMenuItem(
-                value: sugar,
-                child: Text("$sugar sugars"),
-              );
-            }).toList(),
-          ),
+    final user = Provider.of<User>(context);
 
-          //slider
-          Slider(
-            value: (_currentStrength ?? 100).toDouble(),
-            activeColor: Colors.brown[_currentStrength ?? 100],
-            inactiveColor: Colors.brown[_currentStrength],
-            min: 100,
-            max: 900,
-            divisions: 8,
-            onChanged: (val){
-              setState(() {
-                _currentStrength = val.round();
-              });
-            },
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
 
-          ),
+            UserData userData = snapshot.data;
 
-          RaisedButton(
-            color: Colors.pink[400],
-            child: Text(
-              "Update",
-              style: TextStyle(color: Colors.white),
+            return Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text(
+                    "Update your brew settings",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    initialValue: userData.name,
+                    decoration: textInputDecoration,
+                    validator: (val) {
+                      return val.isEmpty ? 'Please enter a name' : null;
+                    },
+                    onChanged: (val) {
+                      setState(() {
+                        _currentName = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  //dropdowm
+                  DropdownButtonFormField(
+                    decoration: textInputDecoration,
+                    value: _currentSugars ?? userData.sugars,
+                    onChanged: (val) {
+                      setState(() {
+                        _currentSugars = val;
+                      });
+                    },
+                    items: sugars.map((sugar) {
+                      return DropdownMenuItem(
+                        value: sugar,
+                        child: Text("$sugar sugars"),
+                      );
+                    }).toList(),
+                  ),
 
-            ),
-            onPressed: () async{
-              print(_currentName);
-              print(_currentSugars);
-              print(_currentStrength);
-            },
-          )
-        ],
-      ),
-    );
+                  //slider
+                  Slider(
+                    value: (_currentStrength ?? userData.strength).toDouble(),
+                    activeColor: Colors.brown[_currentStrength ?? userData.strength],
+                    inactiveColor: Colors.brown[_currentStrength],
+                    min: 100,
+                    max: 900,
+                    divisions: 8,
+                    onChanged: (val) {
+                      setState(() {
+                        _currentStrength = val.round();
+                      });
+                    },
+                  ),
+
+                  RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      "Update",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+                      if(_formKey.currentState.validate()){
+                        await DatabaseService(uid: user.uid).updateUserData(
+                          _currentSugars ?? userData.sugars,
+                          _currentName ?? userData.name,
+                          _currentStrength ?? userData.strength,
+                        );
+                        Navigator.pop(context);
+                      }
+                    },
+                  )
+                ],
+              ),
+            );
+          } else {
+
+            return Loading();
+
+          }
+        });
   }
 }
